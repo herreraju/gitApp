96366b83bc343dd463c223ec432773d8f142bd59
Justin Herrera <herreraju@mullenmail.com>
2015-12-03 14:25:03 -0700

    gitFix

--- scrip.js
+++ scrip.js
@@ -3,13 +3,10 @@ window.onload = res;
 window.onresize = res;
 function res()
 {
+    alert("gdb");
     for(var i = 0; i < document.getElementsByClassName("sec").length; i++)
     {
         document.getElementsByClassName("sec")[i].style.height = ((window.innerHeight).toString() + "px");
     }
-<<<<<<< HEAD
 }
-=======
-    //document.getElementById("pan1").style.left = (((window.innerWidth / 2)  - (parseInt(document.getElementById("pan1").style.width) / 2)).toString() + "px");
-}
->>>>>>> justin
+
